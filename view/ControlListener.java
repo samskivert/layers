a21946b1ce4081742e1421ea7a1a0f834d46ec7d
Marius <marius@cs.washington.edu>
2010-04-06 17:23:39 -0700

    cleanup

--- view/ControlListener.java
+++
@@ -1,7 +0,0 @@
-package view;
-
-public abstract class ControlListener {
-    public abstract void map();
-    // more as needed
-    public abstract void port();
-}
--- view/SuggestionListener.java
+++
@@ -1,6 +0,0 @@
-package view;
-
-public abstract class SuggestionListener {
-    public abstract void yes();
-    public abstract void no();
-}
